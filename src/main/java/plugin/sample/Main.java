ce768928a4bac77399926bd4b1b94a35ebf9887f
Shinya Okada <shinya@okadashinya-no-iMac.local>
2023-02-20 02:42:08 +0900

    hanabi comment change

--- src/main/java/plugin/sample/Main.java
+++ src/main/java/plugin/sample/Main.java
@@ -65,7 +65,7 @@ public final class Main extends JavaPlugin implements Listener {
         firework.setFireworkMeta(fireworkMeta);
       }
       Path path = Path.of("firework.txt");
-      Files.writeString(path, "aho");
+      Files.writeString(path, "boke");
       player.sendMessage(Files.readString(path));
     }
 
